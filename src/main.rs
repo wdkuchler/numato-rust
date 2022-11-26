96bdf359d7ca546e7f5bd6ad1cfcf44d628b6814
Willy Kuchler <wdkuchler@gmail.com>
2022-11-25 22:49:09 -0300

    initial branch

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
 #![allow(unused)]
 
 use std::fs;
-use std::io::prelude::*;
+use std::fs::OpenOptions;
 use std::env;
 use std::{thread, time};
+use std::io::prelude::*;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let porta = std::env::args().nth(1).expect("no pattern given");
     let command = std::env::args().nth(2).expect("no path given");
     let fifty_millis = time::Duration::from_millis(50);
     let thousand_millis = time::Duration::from_millis(1000);
     
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(&porta)
+        .unwrap();
+        
     println!("let's wake up interface");
-    fs::write(&porta, b"\r");
+    writeln!(file, "\r");
     
     thread::sleep(fifty_millis);
 
     if command == "on" {
         println!("let's activate relay");
-        fs::write(&porta, b"relay on 0\r");
+        writeln!(file, "relay on 0\r");
     } else if command == "off" {
         println!("let's deactivate relay");
-        fs::write(&porta, b"relay off 0\r");
+        writeln!(file, "relay off 0\r");
     } else if command == "pulse" {
         println!("let's pulse relay");
-        fs::write(&porta, b"relay on 0\r");
+        writeln!(file, "relay on 0\r");
         thread::sleep(thousand_millis);
-        fs::write(&porta, b"relay off 0\r");
+        writeln!(file, "relay off 0\r");
     } else {
         println!("Error: invalid command argument {}", command); 
     }
+    Ok(())
 }
