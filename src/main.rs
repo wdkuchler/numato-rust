f82c50bbdc5af6ed1077cca13aef839f088fbb6e
Willy Kuchler <wdkuchler@gmail.com>
2022-11-26 09:08:00 -0300

    initial branch

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,63 @@
 #![allow(unused)]
 
+use std::borrow::Borrow;
+use std::fmt::Error;
 use std::fs;
 use std::fs::OpenOptions;
 use std::env;
 use std::{thread, time};
-use std::io::prelude::*;
+use std::io::{prelude::*, Result};
+
+fn wakeup(porta: String) -> std::io::Result<()> {
+    let fifty_millis = time::Duration::from_millis(50);
+
+    let mut file = OpenOptions::new().write(true).append(true).open(&porta).unwrap();
+
+    println!("let's wake up interface");
+    write!(file, "{}", '\r');
+    thread::sleep(fifty_millis);
+    Ok(())
+}
+
+fn activate(porta: String) -> std::io::Result<()> {
+    let mut file = OpenOptions::new().write(true).append(true).open(&porta).unwrap();
+
+    println!("let's activate relay");
+    write!(file, "relay on 0\r");
+    Ok(())
+}
+
+fn deactivate(porta: String) -> std::io::Result<()> {
+    let mut file = OpenOptions::new().write(true).append(true).open(&porta).unwrap();
+
+    println!("let's deactivate relay");
+    write!(file, "relay off 0\r");
+    Ok(())
+}
+
+fn pulse(porta: String) -> std::io::Result<()> {
+    let thousand_millis = time::Duration::from_millis(1000);
+
+    let mut file = OpenOptions::new().write(true).append(true).open(&porta).unwrap();
+
+    println!("let's pulse relay");
+    write!(file, "relay on 0\r");
+    thread::sleep(thousand_millis);
+    write!(file, "relay off 0\r");
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     let porta = std::env::args().nth(1).expect("no pattern given");
     let command = std::env::args().nth(2).expect("no path given");
-    let fifty_millis = time::Duration::from_millis(50);
-    let thousand_millis = time::Duration::from_millis(1000);
-    
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(&porta)
-        .unwrap();
-        
-    println!("let's wake up interface");
-    writeln!(file, "\r");
-    
-    thread::sleep(fifty_millis);
-
-    if command == "on" {
-        println!("let's activate relay");
-        writeln!(file, "relay on 0\r");
-    } else if command == "off" {
-        println!("let's deactivate relay");
-        writeln!(file, "relay off 0\r");
-    } else if command == "pulse" {
-        println!("let's pulse relay");
-        writeln!(file, "relay on 0\r");
-        thread::sleep(thousand_millis);
-        writeln!(file, "relay off 0\r");
-    } else {
-        println!("Error: invalid command argument {}", command); 
-    }
+       
+    wakeup(porta.clone());
+ 
+    match command.as_str() {
+        "on" => activate(porta.clone()),
+        "off" => deactivate(porta.clone()),
+        "pulse" => pulse(porta.clone()),
+        _ => panic!(),
+        };
     Ok(())
 }
